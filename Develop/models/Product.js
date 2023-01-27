ab51c3dc472dad80d05be61a49bc0e6ff1ec6b5e
dylanquaale <dylanquaale33@gmail.com>
2023-01-27 13:49:17 -0600

    Models should all be created

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,4 +1,6 @@
 // import important parts of sequelize library
+const { DECIMAL } = require('sequelize');
+const { INTEGER } = require('sequelize');
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
@@ -9,6 +11,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DECIMAL,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id',
+      },
+    },
+
     // define columns
   },
   {
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
+const inquirer = require("inquirer");
 // import sequelize connection
+const mysql = require("mysql2");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +12,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "raindrop",
+  database: "ecommerce_db",
+});
+
+connection.connect(function (err) {
+  if (err) {
+    return;
+  }
+});
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
