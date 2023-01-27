b9cbc9380141cbb7ade831111fb26d7afe6e8807
dylanquaale <dylanquaale33@gmail.com>
2023-01-27 13:12:23 -0600

    npm i

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const mysql = require("mysql");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,21 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-
-// mysql connection query
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "raindrop",
-  database: "ecommerce_db",
-});
-
-connection.connect(function (err) {
-  if (err) {
-    return;
-  }
-}),
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
