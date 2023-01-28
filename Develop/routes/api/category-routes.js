0c705312fc4a36d06299834733ebce20cabf63d1
dylanquaale <dylanquaale33@gmail.com>
2023-01-28 14:19:52 -0600

    working on the routes now

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -2,15 +2,38 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
+//find all categories
   // be sure to include its associated Products
+
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ include: categoryData }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
+ // find one category by its `id` value
   // be sure to include its associated Products
+
+router.get('/:id', async (req, res) => {
+  try {
+    const driverData = await Driver.findByPk(req.params.id, {
+      include: [{ model: License }, { model: Car }],
+    });
+
+    if (!driverData) {
+      res.status(404).json({ message: 'No driver found with that id!' });
+      return;
+    }
+
+    res.status(200).json(driverData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
